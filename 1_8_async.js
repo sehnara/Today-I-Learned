9a5aa6ba72b54d0429f9dc2f2ba0367d30f1e43a
Sehoon <gsh723@naver.com>
2021-01-18 18:27:01 +0900

    study functions for 'async' like callback, Promise, async and await

---
+++ 1_8_async.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// async & await
+// clear style of using promise = syntatic sugar!
+
+// 1. async
+// 자동으로 promise로 바꿔준다.
+async function fetchUser(){    
+    // do network request in 10 secs....
+    return 'ellie';
+}
+const user = fetchUser();
+console.log(user);
+
+// 2. await ** 기다려!
+// async 함수에서만 쓸 수 있음
+function delay(ms){
+    return new Promise(resolve => resolve => setTimeout(resolve,ms));
+}
+async function getApple(){
+    await delay(2000);
+    return 'apple';
+}
+
+async function getBanana(){
+    await delay(1000);
+    return 'banana';
+}
+
+async function pickFruits(){
+  // async 병렬처리
+  const applePromise = getApple();
+  const bananaPromise = getBanana();
+  const apple = await applePromise;
+  const banana = await bananaPromise;
+  return `${apple} + ${banana}`;
+}
+pickFruits().then(console.log);
+
+// 3. usefuul Promise APIs
+//all : 모든 프로미스들을 기다려준다음 넘어감
+function pickAllFruits(){
+    return Promise.all([getApple(), getBanana()]).then(fruits =>
+        fruits.join('+'));
+}
+pickFruits().then(console.log);
+
+// race 
+function pickOnlyOne(){
+    return Promise.race([getApple(), getBanana()]);
+}
+pickOnlyOne().then(console.log);
